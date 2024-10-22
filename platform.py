c3439b8c4926a0c94e6f8f53830128fafbea4b1e
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-10-22 22:41:00 +1100

    Fix a few typos

--- platform.py
+++ platform.py
@@ -6,13 +6,10 @@ class Platform(pygame.sprite.Sprite):
     """
 
     def __init__(self, x, y, width, height, color):
-        """
-        Create a Platform object.
-        """
-        super().__init__(self, x, y, width, height, color)
+        super().__init__()  # Initialize the Sprite class
         self.image = pygame.Surface((width, height))
         self.image.fill(color)
-        self.rect = self.image.get_rect(top_left=(x, y))
+        self.rect = self.image.get_rect(topleft=(x, y))
 
     # def draw(self, screen):
     #     pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
