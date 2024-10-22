b96c637a569198473922ffb81b75feb01a69d564
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-10-22 22:27:11 +1100

    Refactor Platform class to use pygame's Sprite class - Remove draw(() method

--- platform.py
+++ platform.py
@@ -1,13 +1,18 @@
 import pygame
 
-class Platform:
+class Platform(pygame.sprite.Sprite):
+    """
+    Platform class.
+    """
 
     def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
+        """
+        Create a Platform object.
+        """
+        super().__init__(self, x, y, width, height, color)
+        self.image = pygame.Surface((width, height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(top_left=(x, y))
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+    # def draw(self, screen):
+    #     pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
